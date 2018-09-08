90c423abc24e0881e8013fa3280f955dcfe6de1e
Maken Hubbard <makhub26@outlook.com>
2018-09-08 13:07:13 -0500

    have the list of random numbers created

--- List-RandomSquare/Program.cs
+++ List-RandomSquare/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace List_RandomSquare
 {
@@ -6,7 +7,30 @@ namespace List_RandomSquare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many random numbers do you want to be generated?");
+            int numOfRandNums = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Entering the first number, at do you want the range of these numbers to be (ex. 1 to 10 : first number being 1)");
+            int firstNumOfRange = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Now Enter the second number (ex. 1 to 10 : second number being 10)");
+            int secondNumOfRange = Convert.ToInt32(Console.ReadLine());
+
+            var rand = new Random();
+            var listOfRandomNumbers = new List<int>();
+
+
+            for (int i = numOfRandNums; i > 0; i--)
+            {
+                // for each iteration I want to generate a random number inside the range
+                var randomNum = rand.Next(firstNumOfRange, secondNumOfRange);
+                listOfRandomNumbers.Add(randomNum);
+                Console.WriteLine(listOfRandomNumbers);
+                // add randomly generated number to the list
+            }
+            
+
+
         }
     }
 }
